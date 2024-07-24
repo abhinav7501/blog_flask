8b29598283c2ffd7aaecb04036b5a2e6dc92a3ef
abhinav7501 <abhinav7501@gmail.com>
2024-07-25 04:15:04 +0530

    first commit

---
+++ cleanblog.sql
@@ -0,0 +1,112 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Jul 25, 2024 at 12:40 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `cleanblog`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `contacts`
+--
+
+CREATE TABLE `contacts` (
+  `sno` int(50) NOT NULL,
+  `name` text NOT NULL,
+  `phone` varchar(50) NOT NULL,
+  `mesg` text NOT NULL,
+  `date` datetime DEFAULT current_timestamp(),
+  `email` varchar(50) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `contacts`
+--
+
+INSERT INTO `contacts` (`sno`, `name`, `phone`, `mesg`, `date`, `email`) VALUES
+(1, 'firstpost', '123', 'firstpost', '2024-07-17 19:05:52', 'firstpost@gmail.com'),
+(2, 'Raj aryan', '1234567890', 'first entry', NULL, 'ewdew@gmail.com'),
+(3, 'sda', '1234567890', 'DAafeaefa', NULL, 'daadad@gvc'),
+(4, 'sda', '1234567890', 'DAafeaefa', NULL, 'daadad@gvc'),
+(5, 'paracetamol', '1234567890', 'haan bhai aagya swad', NULL, 'nivedita.gy@rnsit.ac.in');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `sno` int(11) NOT NULL,
+  `title` text NOT NULL,
+  `tagline` text NOT NULL,
+  `slug` varchar(25) NOT NULL,
+  `content` text NOT NULL,
+  `img_file` varchar(12) NOT NULL,
+  `date` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`sno`, `title`, `tagline`, `slug`, `content`, `img_file`, `date`) VALUES
+(1, 'Let\'s Learn Stocks', 'stocks are magic', 'first-post', 'Stocks  consist of all the shares[a] by which ownership of a corporation or company is divided.[1] A single share of the stock means fractional ownership of the corporation in proportion to the total number of shares. This typically entitles the shareholder (stockholder) to that fraction of the company\'s earnings, proceeds from liquidation of assets (after discharge of all senior claims such as secured and unsecured debt),[3] or voting power, often dividing these up in proportion to the number of like shares each stockholder owns. Not all stock is necessarily equal, as certain classes of stock may be issued, for example, without voting rights, with enhanced voting rights, or with a certain priority to receive profits or liquidation proceeds before or after other classes of shareholders.', 'stocks.jpg', '2024-07-22 16:41:58'),
+(2, 'Second Post', 'jinja synopsis updated', 'second-post', 'A Jinja template is simply a text file. Jinja can generate any text-based format (HTML, XML, CSV, LaTeX, etc.). A Jinja template doesn’t need to have a specific extension: .html, .xml, or any other extension is just fine.\r\n\r\nA template contains variables and/or expressions, which get replaced with values when a template is rendered; and tags, which control the logic of the template. The template syntax is heavily inspired by Django and Python.', 'about.png', '2024-07-22 16:38:47'),
+(3, 'new post fix', 'should be added', 'new-post', 'new post is now added', 'img.png', '2024-07-25 04:08:02'),
+(5, 'newpost again', 'new post added', 'fsdfds', 'asfaafaf', 'img.png', '2024-07-22 16:20:43'),
+(6, 'add feature fix', 'should be added', 'fix-post', 'this is a new post with working add feature', 'about.png', '2024-07-25 04:09:14');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `contacts`
+--
+ALTER TABLE `contacts`
+  ADD PRIMARY KEY (`sno`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`sno`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `contacts`
+--
+ALTER TABLE `contacts`
+  MODIFY `sno` int(50) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `sno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
---
+++ main.py
@@ -0,0 +1,195 @@
+from datetime import datetime
+from flask import Flask,render_template, request,session,redirect
+from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import DeclarativeBase
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import String
+from flask_mail import Mail
+import json 
+import os
+import math
+from werkzeug.utils import secure_filename
+
+with open('config.json','r' ) as c:
+    params=json.load(c)["params"]
+    
+local_server=params['local_server']
+
+
+class Base(DeclarativeBase):
+  pass
+
+db = SQLAlchemy(model_class=Base)
+
+
+
+app = Flask(__name__)
+app.secret_key = 'super-secret-key'
+app.config['upload_folder']=params['upload_location']
+
+app.config.update(
+    MAIL_SERVER='smtp.gmail.com',
+    MAIL_PORT='465',
+    MAIL_USE_SSL=True,
+    MAIL_USERNAME=params['gmail-mail'],
+    MAIL_PASSWORD=params['gmail-pass']
+)
+
+mail=Mail(app)
+
+if(local_server):
+ app.config["SQLALCHEMY_DATABASE_URI"] = params['local_uri']
+ 
+else:
+    app.config["SQLALCHEMY_DATABASE_URI"] = params['prod_uri']
+ 
+db.init_app(app)
+
+
+class Contacts(db.Model):
+    sno: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str] = mapped_column(String(50),nullable=False)
+    phone: Mapped[str]=mapped_column(String(12),nullable=False)
+    mesg: Mapped[str]=mapped_column(String(120),nullable=False)
+    date: Mapped[str]=mapped_column(String(12),nullable=True)
+    email:Mapped[str]=mapped_column(String(20),nullable=False)
+    
+    
+    
+class Posts(db.Model):
+    sno: Mapped[int] = mapped_column(primary_key=True)
+    title: Mapped[str] = mapped_column(String(50),nullable=False)
+    slug: Mapped[str]=mapped_column(String(25),nullable=False)
+    content: Mapped[str]=mapped_column(String(120),nullable=False)
+    date: Mapped[str]=mapped_column(String(12),nullable=True)
+    img_file: Mapped[str]=mapped_column(String(25),nullable=False)
+    tagline: Mapped[str]=mapped_column(String(25),nullable=False)
+
+
+@app.route("/")
+def home():
+    posts = Posts.query.filter_by().all()
+    last = math.ceil(len(posts)/int(params['no_of_posts']))
+    page = request.args.get('page')
+    if (not str(page).isnumeric()):
+        page = 1
+    page = int(page)
+    posts = posts[(page-1)*int(params['no_of_posts']):(page-1)*int(params['no_of_posts'])+ int(params['no_of_posts'])]
+    if page==1:
+        prev = "#"
+        next = "/?page="+ str(page+1)
+    elif page==last:
+        prev = "/?page="+ str(page-1)
+        next = "#"
+    else:
+        prev = "/?page="+ str(page-1)
+        next = "/?page="+ str(page+1)
+    
+    return render_template('index.html', params=params, posts=posts, prev=prev, next=next)
+
+@app.route("/edit/<string:sno>",methods=['GET','POST'])
+def edit(sno):
+    if 'user' in session and session['user']==params['admin_user']:
+          if( request.method=='POST'):
+              box_title=request.form.get('title')
+              tline=request.form.get('tline')
+              slug=request.form.get('slug')
+              content=request.form.get('content')
+              img_file=request.form.get('img_file')
+              date=datetime.now()
+              if sno=='0':
+                  post=Posts(title=box_title,slug=slug,content=content,img_file=img_file,tagline=tline,date=date)
+                  db.session.add(post)
+                  db.session.commit()
+              else:
+                  post=Posts.query.filter_by(sno=sno).first()
+                  post.title=box_title
+                  post.slug=slug
+                  post.content=content
+                  post.img_file=img_file
+                  post.tagline=tline
+                  post.date=date
+                  db.session.commit()   
+                  return redirect('/edit/'+sno)
+                   
+    post=Posts.query.filter_by(sno=sno).first()
+    return render_template('edit.html',params=params,post=post,sno=sno)
+
+@app.route("/delete/<string:sno>",methods=['GET','POST'])
+def delete(sno):
+    if 'user' in session and session['user']==params['admin_user']:
+        post=Posts.query.filter_by(sno=sno).first()
+        db.session.delete(post)
+        db.session.commit()
+    return redirect('/dashboard')    
+              
+@app.route("/uploader",methods=['GET','POST'])
+def uploader():
+     if 'user' in session and session['user']==params['admin_user']:
+         if( request.method=='POST'):
+            f=request.files['file1']
+            f.save(os.path.join(app.config['upload_folder'],secure_filename(f.filename)))   
+            return "uploaded"
+                     
+@app.route("/logout")
+def logout():
+    session.pop('user')
+    return  redirect('/dashboard')               
+     
+
+@app.route("/contact",methods=['GET','POST'])
+def contact():
+    
+    
+    if( request.method=='POST'):
+       
+       '''add to database'''
+       name = request.form.get('name')
+       email =request.form.get('email')
+       phone= request.form.get('phone')
+       message=request.form.get('message')
+       
+       entry=Contacts(name=name,phone=phone,mesg=message,email=email,date=datetime.now())
+       db.session.add(entry)
+       db.session.commit()
+       mail.send_message('New message from '+name,
+                         sender=email,
+                         recipients=[params['gmail-mail']],
+                         body=message+"\n"+phone
+           
+       )
+ 
+    return render_template('contact.html',params=params)
+
+@app.route("/about")
+def about():
+    return render_template('about.html',params=params)
+
+@app.route("/dashboard",methods=['GET','POST']  )
+def dashboard():
+    
+    
+    if 'user' in session and session['user']==params['admin_user']:
+        posts=Posts.query.all()
+        return render_template ('dashboard.html',params=params,posts=posts)
+    
+    if (request.method=='POST'):
+        username=request.form.get('uname')
+        userpass=request.form.get('pass')
+        
+        if username==params['admin_user'] and userpass==params['admin_password']:
+            session['user']=username
+            posts=Posts.query.all()
+            return render_template ('dashboard.html',params=params,posts=posts)
+    
+    
+    return render_template('login.html',params=params)
+
+@app.route("/post/<string:post_slug>")
+def post_route(post_slug,methods=['GET']):
+    post=Posts.query.filter_by(slug=post_slug).first()
+    
+    
+    return render_template('post.html',params=params,post=post)
+
+app.run(debug=True)
